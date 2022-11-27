4779730eafe78a387306b9113527ba3f76a2d76f
Adithya Marayil <adithyamarayil@GMG.LOCAL>
2022-11-28 00:21:48 +0530

    interface

---
+++ src/app/charts/charts.interfaces.ts
@@ -0,0 +1,9 @@
+export interface IChartData{
+    labels?:Array<string>,
+    title?:string,
+    data:Array<Array<number>>,
+    gridOptionsX?:boolean,
+    gridOptionsY?:boolean,
+    type:Array<string>,
+    backgroundColors:Array<string>
+}
